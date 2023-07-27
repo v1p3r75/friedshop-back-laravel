fea0fef700b28b7d6f5baf6f9f7022b64733f172
v1p3r75 <elfriedv16@gmail.com>
2023-07-27 13:43:23 +0100

    Updated allowed host

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowedMethods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://friedshop.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
